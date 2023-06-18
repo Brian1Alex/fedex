fb10761a677c2c46cf35fd84880af5edacdac109
Brian1Alex <brian.bacs25@gmail.com>
2023-06-18 12:56:23 -0500

    Agregando Plantilla

---
+++ application/views/footer.php
@@ -0,0 +1,46 @@
+ <style>
+    .tit{
+        color: aliceblue;
+    }
+
+ </style>
+    
+ 
+ <!-- footer section start -->
+ <div class="footer_section layout_padding">
+     <div class="container">
+         <div class="" Align="center">
+             <h1 class="tit">Confia tus envios mediante FedEx</h1>
+             <p>La mejor compañia en servicio de envios a nivel nacional e internacional</p>
+             <hr class="lin">
+         </div>
+
+         <div class="social_icon">
+             <ul>
+                 <li><a href="https://www.facebook.com/FedExLATAM/"><img src="<?php echo base_url(); ?>/plantilla/images/fb-icon.png"></a></li>
+                 <li><a href="https://twitter.com/fedexteayuda"><img src="<?php echo base_url(); ?>/plantilla/images/twitter-icon.png"></a></li>
+                 <li><a href="https://web.telegram.org/"><img src="<?php echo base_url(); ?>/assets/images/telegram-icon.png"></a></li>
+                 <li><a href="https://www.instagram.com/fedex/"><img src="<?php echo base_url(); ?>/plantilla/images/instagram-icon.png"></a></li>
+             </ul>
+         </div>
+     </div>
+ </div>
+ <!-- footer section end -->
+
+ <!-- Javascript files-->
+ <script src="<?php echo base_url(); ?>/plantilla/js/jquery.min.js"></script>
+ <script src="<?php echo base_url(); ?>/plantilla/js/popper.min.js"></script>
+ <script src="<?php echo base_url(); ?>/plantilla/js/bootstrap.bundle.min.js"></script>
+ <script src="<?php echo base_url(); ?>/plantilla/js/jquery-3.0.0.min.js"></script>
+ <script src="<?php echo base_url(); ?>/plantilla/js/plugin.js"></script>
+ <!-- sidebar -->
+ <script src="<?php echo base_url(); ?>/plantilla/js/jquery.mCustomScrollbar.concat.min.js"></script>
+ <script src="<?php echo base_url(); ?>/plantilla/js/custom.js"></script>
+ <!-- javascript -->
+ <script src="<?php echo base_url(); ?>/plantilla/js/owl.carousel.js"></script>
+ <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
+
+
+ </body>
+
+ </html>
