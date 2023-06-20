4937fc6883d30fa837746f3770434a4360f6e7c8
ANDYNEITOR04 <cumbajinandy@gmail.com>
2023-06-20 12:40:34 -0500

    Modificacion Pagina Contactos/ Interfaz

--- application/views/contactanos/iniCon.php
+++ application/views/contactanos/iniCon.php
@@ -1,20 +1,26 @@
-<div class="services_section layout_padding">
+
       <div class="container" Align="center">
          <h1 class="services_taital">Contactos </h1>
-         <p class="services_text">Disponoble en</p>
+         <p class="services_text">Disponoble solo en horarios laborales (07:00am-18:00pm) <br> Gracias por su conprencion :D </p>
          <div class="services_section_2">
             <div class="row">
-               <div class="col-md-4">
-                  <div><img src="<?php echo base_url(); ?>/assets/images/agCli.gif" class="services_img"></div>
-                  <div class="btn_main"><a href="<?php echo site_url() ?>/pedidos/agrePedi">Agregar</a></div>
-               </div>
-               <div class="col-md-4">
-                  <div><img src="<?php echo base_url(); ?>/assets/images/lisCli.gif" class="services_img"></div>
-                  <div class="btn_main active"><a href="<?php echo site_url() ?>/pedidos/listarPedi">Listar</a></div>
+               <div class="col-md-8">
+               <p class="about_taital"> Numeros Telefonicos</p> <br> <br>
+               <p class="alert alert-dark" role="alert">Telefono Convencional <a href="#" class="alert-link">022222-2222</a>
+            <br>Telefonos celulares <a href="#" class="alert-link">0987654321-0912345678</a>
+            </p>
+            <p class="about_taital"> Correos Electronicos</p>  <br> <br>
+            <p class="alert alert-primary" role="alert">Gmail: <a href="#" class="alert-link">fedex.ecuador@gmail.com</a>
+            <br>Hotmail: <a href="#" class="alert-link">fedex.ecuador@hotmail.com</a>
+            </p>
+            <p class="about_taital"> DIRECCION</p>  <br> <br>
+            <p class="alert alert-success" role="alert">Quito: <a href="#" class="alert-link">RGFF+284, Av. Naciones Unidas, Quito 170135</a>
+            </p>
+            <p class="about_taital"> Otras Sucursales =></p>
                </div>
                <div class="col-md-4">
                   <div><img src="<?php echo base_url(); ?>/assets/images/pato.gif" class="services_img" Alt="ubicaciones"></div>
-                  <div class="btn_main"><a href="<?php echo site_url() ?>/pedidos/desPedi">Destinos</a></div>
+                  <div class="btn_main"><a href="<?php echo site_url() ?>/pedidos/desPedi">Sucursales</a></div>
                </div>
             </div>
          </div>
